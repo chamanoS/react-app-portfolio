ddb72dc69936bcffdb71baaac56a3727500f95c9
Chamano Ramashia <chamano.sr@gmail.com>
2023-06-07 10:27:03 +0200

    refactoring code

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { AiFillAppstore } from "react-icons/ai";
-import { FaMobile, FaGlobe } from "react-icons/fa";
-import { SiProgress, SiAntdesign } from "react-icons/si";
+import { FaGlobe } from "react-icons/fa";
+import {  SiAntdesign } from "react-icons/si";
 import Title from "../layouts/Title";
 import Card from "./Card";
 
@@ -22,16 +22,7 @@ const Features = () => {
           des="We provide custom app development tailored to meet the unique needs of the client and are designed to be user-friendly and functional."
           icon={<AiFillAppstore />}
         />
-        <Card
-          title="Consultation"
-          des="Our consulting services cover technology, architecture, performance, security, and project management. We provide advice to help clients achieve optimal results in their software projects."
-          icon={<SiProgress />}
-        />
-        <Card
-          title="Digital Marketing"
-          des="We offer a range of digital marketing services including SEO, PPC advertising, social media marketing, content marketing, email marketing, CRO, and analytics."
-          icon={<FaMobile />}
-        />
+      
         <Card
           title="Content Writing"
           des="Our content is designed to help businesses create high-quality, engaging, and relevant content for their websites, blogs, and social media platforms. We create content that resonates with your target audience."
--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,11 +1,10 @@
 import React, {  useState } from 'react'
 import Title from '../layouts/Title';
-import Education from './Education';
 import Skills from './Skills';
 import Experience from "./Experience"
 
 const Resume = () => {
-   const [educationData, setEducationData] = useState(true);
+
    const [skillData, setSkillData] = useState(false);
    const [experienceData, setExperienceData] = useState(false);
   return (
@@ -17,21 +16,6 @@ const Resume = () => {
         <ul className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
           <li
             onClick={() =>
-              setEducationData(true) &
-              setSkillData(false) &
-              setExperienceData(false) 
-            }
-            className={`${
-              educationData
-                ? "border-designColor rounded-lg"
-                : "border-transparent"
-            } resumeLi`}
-          >
-            Education
-          </li>
-          <li
-            onClick={() =>
-              setEducationData(false) &
               setSkillData(true) &
               setExperienceData(false)
             }
@@ -43,7 +27,6 @@ const Resume = () => {
           </li>
           <li
             onClick={() =>
-              setEducationData(false) &
               setSkillData(false) &
               setExperienceData(true) 
             }
@@ -57,7 +40,6 @@ const Resume = () => {
           </li>
         </ul>
       </div>
-      {educationData && <Education />}
       {skillData && <Skills />}
       {experienceData && <Experience />}
  
